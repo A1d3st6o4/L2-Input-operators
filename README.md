b2c9b5c41ad39776eeb46a0e8618d602d81a22c1
Aiden Estrada <107879431+A1d3st6o4@users.noreply.github.com>
2022-06-20 19:03:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Input
+Input - How the computer gets inforamation from the computer 
+
+Note: Python initially treats iunput as a `string` type
+
+Ex. 
+
+5 --> `5`
+
+Today is Monday --> "TOday is monday"
+
+### Formula
+`variable name = input("prompt")`
+
+PROMPT - A question or statement that the computer asks the user
+
+Python stores the __INPUT__ in the __variablename__
+
+## 
---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+Username = input("Enter your name: ")
+#print ("Hey " + Username)
+usergrade = input("What grade are you in?")
+print("hey " + Username + "," + usergrade + "Is a lame grade")
+
+useryear = int(input("what year were you born in?"))
+
+userage = 2022 - useryear 
+print(str(userage) + "is so young!")
+
+'''
+
+#create your own madlibs 
+# have at least 5 inputs 
+
+
+
+
+question1 = input ("Who is your Dream man?: ")
+
+print("ew not him! ")
+
+question2 = input ("Why do you like him so much?: ")
+
+print("Huh, " + question1 + " looks ugly to me but I wont judge.")
+
+question3 = input ("what is that")
+
+print("why do you have a " + question3 + "?")
+
+narrate1 = ("Im going to go now, want to come?: ")
+
+print("ok fine then be lilke that")
